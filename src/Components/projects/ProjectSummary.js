5e0dede77e3c883c932e6aad84d69f965c09bd50
Aahan Singh Charak <aahancharak@gmail.com>
2020-09-17 17:29:28 +0530

    Added project summary section

---
+++ src/Components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card-content grey-text text-darken-3">
+      <span className="card-title">Project Title</span>
+      <p>Posted by net Samurai</p>
+      <p className="grey-text">3rd September 2am</p>
+    </div>
+  );
+};
+
+export default ProjectSummary;
