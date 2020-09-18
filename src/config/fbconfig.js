2b3521eb98c178ffa2a0a1d4cf485f98f6ea3b7a
Aahan Singh Charak <aahancharak@gmail.com>
2020-09-18 23:40:51 +0530

    Added firestore to the project

---
+++ src/config/fbconfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDRgZvA6C7qf0KwOvrE7277BgrONmjpEcE",
+  authDomain: "project-manager-dcc76.firebaseapp.com",
+  databaseURL: "https://project-manager-dcc76.firebaseio.com",
+  projectId: "project-manager-dcc76",
+  storageBucket: "project-manager-dcc76.appspot.com",
+  messagingSenderId: "277399902848",
+  appId: "1:277399902848:web:78ddf65ccadffd42232465",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
