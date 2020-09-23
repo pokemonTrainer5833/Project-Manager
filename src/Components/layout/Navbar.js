c55ba183ce28307b33b31484f6ca08cf79e97187
Aahan Singh Charak <aahancharak@gmail.com>
2020-09-24 02:55:08 +0530

    Added the login status section

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
-
+import { connect } from "react-redux";
 const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
@@ -18,4 +18,8 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {};
+};
+export default connect(mapStateToProps)(Navbar);
