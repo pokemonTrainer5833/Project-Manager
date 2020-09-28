be3e40f6faf0d76c73e09a8a72d01324d083fda1
Aahan Singh Charak <aahancharak@gmail.com>
2020-09-28 18:55:03 +0530

    Added the project creation with details section

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -4,13 +4,15 @@ export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore
       .collection("projects")
       .add({
         ...project,
-        authorFirstName: "shadow",
-        authorLastName: "warrior",
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date(),
       })
       .then(() => {
